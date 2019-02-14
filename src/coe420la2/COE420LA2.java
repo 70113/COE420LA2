fc1f1e83ffe79e8425918b68c5b4123506901965
70113 <b00070113@aus.edu>
2019-02-14 15:53:24 +0400

    Initial Commit

---
+++ src/coe420la2/COE420LA2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420la2;
+
+/**
+ *
+ * @author b00070113
+ */
+public class COE420LA2 {
+
+      public static void func1(){
+        System.out.println("HELLO WORLD");
+    }
+   
+     
+    public static void main(String[] args) {
+        // TODO code application logic here
+        func1();
+    }
+    
+}
